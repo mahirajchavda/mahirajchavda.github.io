3dfa8014fe2bd8c4e8b4cbb2598120fc6f2c8f99
Jemish <jemish@logisticinfotech.co.in>
2021-09-21 17:47:40 +0530

    add build

---
+++ precache-manifest.cbb4c9aeaf6cfa3f222aecaa20f66dd1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edec6be549171b819201a14c1971d4b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04c939bf2977e018d89b",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "04c939bf2977e018d89b",
+    "url": "/static/js/2.df8f9b1a.chunk.js"
+  },
+  {
+    "revision": "e0bf82e748d52d9423be8105742139b9",
+    "url": "/static/js/2.df8f9b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b815d0f79d2338137689",
+    "url": "/static/js/main.82e226ff.chunk.js"
+  },
+  {
+    "revision": "3323fa64bd8fcf4d2fba",
+    "url": "/static/js/runtime-main.f5b3a2a1.js"
+  },
+  {
+    "revision": "1e1fe7050fc9d37f3091fa0f40c7fb13",
+    "url": "/static/media/instagram.1e1fe705.png"
+  },
+  {
+    "revision": "78a01716701d3cf09e3958cf6ae245ca",
+    "url": "/static/media/universily-header-logo.78a01716.jpeg"
+  },
+  {
+    "revision": "70b02ffd6167df958c9e273b3b57e349",
+    "url": "/static/media/youtube.70b02ffd.png"
+  }
+]);
